f4c14a6733456026c60ee0a2a58130f0d76c5299
tanzl <tanzl_xyy@tanzls-MacBook-Air.local>
2023-08-13 16:57:27 +0800

    simple use of stablecode

--- ftbert/main.py
+++ ftbert/main.py
@@ -2,37 +2,39 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-from torch import nn
-import transformers
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import torch
-#print(torch.__version__)
+# loading dataset from hugging face
 from datasets import load_dataset
+from transformers import AutoTokenizer
 emotions = load_dataset('emotion')
 emotions_df = pd.DataFrame.from_dict(emotions['train'])
-#print(emotions['train'][:5])
-from transformers import AutoTokenizer
+# tokenize the dataset
+
+
 model_ckpt = 'distilbert-base-uncased'
 tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
 encode_text = tokenizer.encode(emotions_df.iloc[6324]['text'])
-#print(encode_text)
+
+
+# print(encode_text)
+# tokenize each batch
 def batch_tokenize(batch):
     return tokenizer(batch['text'], padding=True, truncation=True)
-emotions_encoded = emotions.map(batch_tokenize,batched=True,batch_size=None)
-#print(emotions_encoded['train'].features['text'])
 
+
+emotions_encoded = emotions.map(batch_tokenize, batched=True, batch_size=None)
+# print(emotions_encoded['train'].features['text'])
 emotions_encoded.set_format('torch', columns=['label', 'input_ids', 'attention_mask'])
-#print(type(emotions_encoded['train']['text']))
-
+# print(type(emotions_encoded['train']['text']))
+# import pre-trained bert model from hugging face
 from transformers import AutoModel
+
 model = AutoModel.from_pretrained(model_ckpt)
 print(model)
 from transformers import TrainingArguments, Trainer
+
 batch_size = 64
-logging_steps = len(emotions_encoded['train'])//batch_size
+logging_steps = len(emotions_encoded['train']) // batch_size
 model_name = f'{model_ckpt}_emotion_ft_0416'
 training_args = TrainingArguments(output_dir=model_name,
                                   num_train_epochs=4,
@@ -47,21 +49,11 @@ training_args = TrainingArguments(output_dir=model_name,
                                   push_to_hub=False,
                                   log_level="error")
 from transformer_utils import compute_classification_metrics
+
 trainer = Trainer(model=model,
-                  tokenizer=tokenizer,
+                  args=training_args,
                   train_dataset=emotions_encoded['train'],
                   eval_dataset=emotions_encoded['validation'],
-                  args=training_args,
                   compute_metrics=compute_classification_metrics)
-trainer.train()
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
